da444cf121a7ce9f2eeb2b8173090173b539f0bd
Zac Smith <zrs3141592@gmail.com>
2019-11-06 20:49:23 -0700

    heroku build

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const server = require("./api/server.js");
 
 // const { port } = process.env;
+// destructure is not best option
 
 console.log(process.env.USER);
 console.log(process.env.PORT);
@@ -9,3 +11,6 @@ const PORT = process.env.PORT || 4444;
 server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
+
+// const sortField = req.query.sortBy || 'id'
+// const sortField = req.query.sortBy ? req.query.sortBy : 'id'
--- notes.md
+++ notes.md
@@ -5,3 +5,7 @@
 - add `process.env.PORT` index.js
 - CICD Continuos integration Continuous deployment
 - add to package.json `"start": "node index.js"`
+- `const sortField = req.query.sortBy || 'id'`
+- 12 Factor App Development
+- `npm i dotenv`
+- very top of index.js, top of app `require('dotenv').config()`
